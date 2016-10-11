0c17a3c7800bcba1cb71807f0d469ebe0e53aa95
Lê Việt Hồng <suoinguon@users.noreply.github.com>
2016-10-11 18:07:26 +0700

    url-re-write

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,5 +1,16 @@
 location / {
-    index  index.php index.html index.htm;
+    # try to serve file directly, fallback to rewrite
+    try_files $uri @rewriteapp;
+}
+
+location @rewriteapp {
+    # rewrite all to app.php
+    rewrite ^(.*)$ /index.html/$1 last;
+}
+
+location ~ ^/(app|app_dev|config)\.php(/|$) {
+    try_files @heroku-fcgi @heroku-fcgi;
+    internal;
 }
 
 # for people with app root as doc root, restrict access to a few things
